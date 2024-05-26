4fbb24367244a4a0a5c99fdb8def2636fd92e98e
Kartik Singh <kartik.ynwa@gmail.com>
2024-05-27 00:00:05 +0530

    Update copying code to work properly

--- src/copy.js
+++ src/copy.js
@@ -3,16 +3,18 @@
     const selection = window.getSelection();
     const range = document.createRange();
     const dd = this.querySelector('dd');
+    const copyText = this.querySelector('.copy-text');
 
     if (dd.classList.contains('success')) {
       return;
     }
 
-    range.selectNodeContents(dd.childNodes[0]);
+    copyText.style.display = "block";
+    range.selectNodeContents(copyText.childNodes[0]);
     selection.removeAllRanges();
     selection.addRange(range);
-
     document.execCommand('copy');
+    copyText.style.display = "none";
     selection.removeAllRanges();
 
     const original = dd.textContent;
